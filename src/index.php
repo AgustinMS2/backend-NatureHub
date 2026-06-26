be16a6dff8c00dcdbcea859849250e35e5c91b31
Agustin Machado <agusm.s.2006@gmail.com>
2026-06-26 01:03:22 -0300

    . Co-authored-by: Luca Crespi <Cacacacaiii@users.noreply.github.com>

--- src/index.php
+++ src/index.php
@@ -7,7 +7,7 @@ header("Access-Control-Allow-Headers: Content-Type, X-Auth-Token");
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
-}
+}       
 
 include __DIR__ . "/logica/endpoints/UsuarioEndpoint.php";
 include __DIR__ . "/logica/endpoints/PublicacionEndpoint.php";
